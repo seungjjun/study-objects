c281c8d10ba3867b17d8a2dfc6897cbccfd73ee7
seungjjun <il1uso@naver.com>
2023-09-14 20:11:25 +0900

    refactor movie reservation code

--- playground/chapter04/app/src/main/java/chapter04/Movie.java
+++ playground/chapter04/app/src/main/java/chapter04/Movie.java
@@ -1,6 +1,7 @@
 package main.java.chapter04;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Movie {
@@ -13,43 +14,47 @@ public class Movie {
     private Money discountAmount;
     private double discountPercent;
 
-    public Money getFee() {
-        return fee;
-    }
-
-    public void setFee(Money fee) {
-        this.fee = fee;
-    }
-
-    public List<DiscountCondition> getDiscountConditions() {
-        return discountConditions;
-    }
-
-    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
-        this.discountConditions = discountConditions;
-    }
-
     public MovieType getMovieType() {
         return movieType;
     }
 
-    public void setMovieType(MovieType movieType) {
-        this.movieType = movieType;
+    public Money calculateAmountDiscountedFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(discountAmount);
     }
 
-    public Money getDiscountAmount() {
-        return discountAmount;
+    public Money calculatePercentDiscountedFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(fee.times(discountPercent));
     }
 
-    public void setDiscountAmount(Money discountAmount) {
-        this.discountAmount = discountAmount;
+    public Money calculateNoneDiscountedFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee;
     }
 
-    public double getDiscountPercent() {
-        return discountPercent;
-    }
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
+        for (DiscountCondition condition : discountConditions) {
+            if (condition.getType() == DiscountConditionType.PERIOD) {
+                if (condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) {
+                    return true;
+                }
+            } else {
+                if (condition.isDiscountable(sequence)) {
+                    return true;
+                }
+            }
+        }
 
-    public void setDiscountPercent(double discountPercent) {
-        this.discountPercent = discountPercent;
+        return false;
     }
 }
--- playground/chapter04/app/src/main/java/chapter04/Screening.java
+++ playground/chapter04/app/src/main/java/chapter04/Screening.java
@@ -1,34 +1,35 @@
 package main.java.chapter04;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Screening {
-    private Movie movie;
-    private int sequence;
-    private LocalDateTime whenScreened;
+    private final Movie movie;
+    private final int sequence;
+    private final LocalDateTime whenScreened;
 
-    public Movie getMovie() {
-        return movie;
-    }
-
-    public void setMovie(Movie movie) {
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
         this.movie = movie;
-    }
-
-    public int getSequence() {
-        return sequence;
-    }
-
-    public void setSequence(int sequence) {
         this.sequence = sequence;
-    }
-
-    public LocalDateTime getWhenScreened() {
-        return whenScreened;
-    }
-
-    public void setWhenScreened(LocalDateTime whenScreened) {
         this.whenScreened = whenScreened;
+    }
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT -> {
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+            }
+            case PERCENT_DISCOUNT -> {
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+            }
+            case NONE_DISCOUNT -> {
+                return movie.calculateNoneDiscountedFee().times(audienceCount);
+            }
+        }
+
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
     }
 }
