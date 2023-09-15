b80cc35719c44fccc8acb1f0ca3e569262145359
seungjjun <il1uso@naver.com>
2023-09-15 23:10:35 +0900

    refactor movie discount amount code

--- playground/chapter05/app/src/main/java/chapter05/Movie.java
+++ playground/chapter05/app/src/main/java/chapter05/Movie.java
@@ -1,17 +1,21 @@
 package main.java.chapter05;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
-public class Movie {
+public abstract class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
     private List<DiscountCondition> discountConditions;
 
-    private MovieType movieType;
-    private Money discountAmount;
-    private double discountPercent;
+    public Movie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = Arrays.asList(discountConditions);
+    }
 
     public Money calculateMovieFee(Screening screening) {
         if (isDiscountable(screening)) {
@@ -25,25 +29,9 @@ public class Movie {
         return discountConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
 
-
-    private Money calculateDiscountAmount() {
-        return switch (movieType) {
-            case AMOUNT_DISCOUNT -> calculateAmountDiscountAmount();
-            case PERCENT_DISCOUNT -> calculatePercentDiscountAmount();
-            case NONE_DISCOUNT -> calculateNoneDiscountAmount();
-        };
-
+    protected Money getFee() {
+        return fee;
     }
 
-    private Money calculateAmountDiscountAmount() {
-        return discountAmount;
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.times(discountPercent);
-    }
-
-    private Money calculateNoneDiscountAmount() {
-        return Money.ZERO;
-    }
+    abstract protected Money calculateDiscountAmount();
 }
