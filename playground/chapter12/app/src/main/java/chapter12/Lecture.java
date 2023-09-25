8078d54f318525be88a0d149b9b2615931321d6c
seungjjun <il1uso@naver.com>
2023-09-25 21:19:24 +0900

    add polymorphism example code

---
+++ playground/chapter12/app/src/main/java/chapter12/Lecture.java
@@ -0,0 +1,37 @@
+package main.java.chapter12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lecture {
+    private int pass;
+    private String title;
+    private List<Integer> scores = new ArrayList<>();
+
+    public Lecture(int pass, String title, List<Integer> scores) {
+        this.pass = pass;
+        this.title = title;
+        this.scores = scores;
+    }
+    public double average() {
+        return scores.stream()
+                .mapToInt(Integer::intValue)
+                .average().orElse(0);
+    }
+
+    public List<Integer> getScores() {
+        return scores;
+    }
+
+    public String evaluate() {
+        return String.format("Pass:%d Fail:%d", passCount(), failCount());
+    }
+
+    private long passCount() {
+        return scores.stream().filter(scores -> scores >= pass).count();
+    }
+
+    private long failCount() {
+        return scores.size() - passCount();
+    }
+}
